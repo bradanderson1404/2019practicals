ee8f8d2acbe9e664579d3054b7e0080234e2aa32
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2019-08-29 02:47:25 +1000

    Refactor conversion equations

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -6,18 +6,28 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = convert_celsius()
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit()
             print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
         print(menu)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def convert_fahrenheit():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def convert_celsius():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
 
 
 main()
