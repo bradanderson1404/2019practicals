7dfc9282fc00b51a96a754819d92ab03ae45d87a
bradanderson1404 <bradley.anderson1@my.jcu.edu.au>
2019-08-29 02:36:58 +1000

    Refactor password input

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -1,11 +1,16 @@
 def main():
     min_length = int(input("Enter Password Length: "))
+    password = get_password(min_length)
+    for char in password:
+        print("*", end='')
+
+
+def get_password(min_length):
     password = input("Enter Password: ")
     while len(password) < min_length:
         print("Password is too short.")
         password = input("Enter Password: ")
-    for char in password:
-        print("*", end='')
+    return password
 
 
 main()
